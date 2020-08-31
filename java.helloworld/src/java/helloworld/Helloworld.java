27eeac1639c52f3edb88c83ff4b76b7a6efe0065
alessandro <alessandro.feri@stud.unifi.it>
2020-08-31 19:51:47 +0200

    Something on main method on the master branch

--- java.helloworld/src/java/helloworld/Helloworld.java
+++ java.helloworld/src/java/helloworld/Helloworld.java
@@ -2,9 +2,10 @@ package java.helloworld;
 
 public class Helloworld {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Hello World");
 	}
 
 }
